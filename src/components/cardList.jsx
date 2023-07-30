7e59ce44b5336ebaee8cf78a81d411e20ee4d419
Thiago Scarmagnani Arques <thiagoarques2@gmail.com>
2023-07-30 14:55:22 -0300

    Finalizado

--- src/components/cardList.jsx
+++ src/components/cardList.jsx
@@ -7,7 +7,14 @@ export const CardList = ({ cards, handleDeleteCard }) => {
       <h4>Resumo Financeiro</h4>
       <ul className={styleListCard.listCard}>
         {cards.map((card) => (
-          <li className={`${styleCard.cards} ${card.type === "Entrada" ? styleCard.greenBorder : styleCard.grayBorder}`} key={card.id}>
+          <li
+            className={`${styleCard.cards} ${
+              card.type === "Entrada"
+                ? styleCard.greenBorder
+                : styleCard.grayBorder
+            }`}
+            key={card.id}
+          >
             <div>
               <h2 className={styleCard.des}>{card.des}</h2>
               <p className={styleCard.type}>{card.type}</p>
@@ -19,7 +26,12 @@ export const CardList = ({ cards, handleDeleteCard }) => {
                   currency: "BRL",
                 })}
               </p>
-              <button className={styleCard.buttonRemove} onClick={() => handleDeleteCard(card.id)}>Excluir</button>
+              <button
+                className={styleCard.buttonRemove}
+                onClick={() => handleDeleteCard(card.id)}
+              >
+                Excluir
+              </button>
             </div>
           </li>
         ))}
